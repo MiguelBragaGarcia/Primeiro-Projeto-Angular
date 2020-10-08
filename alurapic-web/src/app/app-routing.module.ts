1500d57b180c8e58d90d28759e680d7cf852fc2a
Miguel Braga Garcia <miguelbragagarcia@gmail.com>
2020-10-08 20:09:05 -0300

    Adicionando roteamento a aplicação

--- alurapic-web/src/app/app-routing.module.ts
+++ alurapic-web/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
-const routes: Routes = [];
+
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+
+//Aqui é onde fica as rotas de nossa aplicação
+const routes:Routes = [
+{path:'user/:userName',  component: PhotoListComponent},
+{path:'photo/add', component: PhotoFormComponent},
+{path:'**', component: NotFoundComponent}
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
+
--- alurapic-web/src/app/app.module.ts
+++ alurapic-web/src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './app-routing.module';
 import { PhotosModule } from './photos/photos.module';
-
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     PhotosModule,
-
+    AppRoutingModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
